3186ed5cb885e1e571dcffe4add262111978cd64
Gustavo Santos Arruda <gsarruda.office@gmail.com>
2025-06-30 17:16:08 -0300

    feat: implement scheduling functionality with API integration and display of daily schedules

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,7 @@
 import dayjs from "dayjs";
 
+import { scheduleNew } from "../../services/schedule-new";
+
 const form = document.querySelector("form");
 const selectedDate = document.getElementById("date");
 const clientName = document.getElementById("client");
@@ -13,7 +15,7 @@ selectedDate.value = inputToday;
 // Define a data mínima como sendo a data atual
 selectedDate.min = inputToday;
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault(); // Prevent the default form submission behavior
 
   try {
@@ -40,7 +42,7 @@ form.onsubmit = (event) => {
     // Gera um ID
     const id = new Date().getTime();
 
-    console.log({ id, name, when });
+    await scheduleNew({ id, name, when });
   } catch (error) {
     alert("Não foi possível realizar o agendamento");
     console.log(error);
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,12 +1,19 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
 import { hoursLoad } from "../form/hours-load";
+import { schedulesShow } from "../schedules/show.js";
 
 // Seleciona o input de data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   // Obtem a data do input
   const date = selectedDate.value;
   // Busca na API os agendamentos para carregar do lado direito da página
+  const dailySchedules = await scheduleFetchByDay({ date });
+
+  // Exibe os agendamentos
+  schedulesShow({ dailySchedules });
+
   // Os horários disponíveis (horário futuro + não agendados) do lado esquerdo (form)
   hoursLoad({ date });
 }
